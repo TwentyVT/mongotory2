12007be251cfb9c93565236d6ce9694d2502a3be
TwentyVT <sonatore@amazon.com>
2019-01-22 20:58:02 -0500

    fixing routes

--- server.js
+++ server.js
@@ -2,57 +2,43 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
+
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongotorium";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// Routes
 app.get("/", function (req, res) {
-  // First, we grab the body of the html with axios
-
-  // Send a message to the client
+  
   res.sendfile("index.html");
 });
-// A GET route for scraping the echoJS website
+
 app.get("/scrape", function (req, res) {
-  // First, we grab the body of the html with axios
+
   axios.get("http://digg.com").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+
     var $ = cheerio.load(response.data);
-    // Now, we grab every h2 within an article tag, and do the following:
+
     $(".digg-story__content").each(function (i, element) {
-      // Save an empty result object
+
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
+
       result.title = $(this)
-        // .children(".digg-story__header")
-        // .children(".digg-story__title entry-title")
+        
         .find(".digg-story__title-link")
         .text()
         .trim();
@@ -63,38 +49,38 @@ app.get("/scrape", function (req, res) {
         .trim();
       console.log(result.link)
 
-      // Create a new Article using the `result` object built from scraping
+
       db.Article.create(result)
         .then(function (dbArticle) {
-          // View the added result in the console
+
           console.log(dbArticle);
         })
         .catch(function (err) {
-          // If an error occurred, log it
+
           console.log(err);
         });
     });
 
-    // Send a message to the client
+
     res.send("Scrape Complete");
   });
 });
 
-// Route for getting all Articles from the db
+
 app.get("/articles", function (req, res) {
-  // Grab every document in the Articles collection
+
   db.Article.find({})
     .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
+
       res.json(dbArticle);
     })
     .catch(function (err) {
-      // If an error occurred, send it to the client
+
       res.json(err);
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
+
 app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
