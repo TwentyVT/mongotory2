ba08a5e78b528a7af2bf3fd9ad6ef9bdbd9a651a
TwentyVT <sonatore@amazon.com>
2019-01-22 20:35:36 -0500

    finishing

--- public/app.js
+++ public/app.js
@@ -1,5 +1,16 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-$()
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 8080;
+
+var app = express();
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
     // For each one
@@ -9,7 +20,45 @@ $.getJSON("/articles", function(data) {
     }
   });
   
-  
+  $("#scraper").on("click", function (){
+
+    axios.get("http://digg.com").then(function (response) {
+
+    var $ = cheerio.load(response.data);
+
+    $(".digg-story__content").each(function (i, element) {
+
+      var result = {};
+
+
+      result.title = $(this)
+        
+        .find(".digg-story__title-link")
+        .text()
+        .trim();
+      console.log("THIS IS A TITLE" + result.title) 
+      result.link = $(this)
+        .find(".digg-story__description")
+        .text()
+        .trim();
+      console.log(result.link)
+
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+
+          console.log(err);
+        });
+    });
+
+
+    res.send("Scrape Complete");
+  });
+  })
   // Whenever someone clicks a p tag
   $(document).on("click", "p", function() {
     // Empty the notes from the note section
